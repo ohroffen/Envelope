5df85411bc78c41d169f3c0addc7376b20737f26
cmhzc <chmhzc@126.com>
2021-11-08 18:52:44 +0800

    feature: kafka producer

---
+++ mq/mq.go
@@ -0,0 +1,39 @@
+package mq
+
+import (
+	"MyEnvelope/entity"
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var w *kafka.Writer
+
+func Mq_init() {
+	w = &kafka.Writer{
+		Addr:     kafka.TCP(os.Getenv("KAFKA_HOST")),
+		Topic:    os.Getenv("KAFKA_TOPIC"),
+		Balancer: &kafka.CRC32Balancer{Consistent: false},
+		Async:    false, // wait to be tested
+	}
+}
+
+func Send_message(envelope *entity.Envelope) {
+	msg, err := json.Marshal(envelope)
+	if err != nil {
+		log.Fatal(err)
+	}
+	key := make([]byte, 8)
+	// put UserID of int64 into 8 byte
+	binary.LittleEndian.PutUint64(key, uint64(envelope.UserID))
+	w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   key,
+			Value: msg,
+		},
+	)
+}
